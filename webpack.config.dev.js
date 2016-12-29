4d16a9d4c0b4a630a584428d5306c6588c410820
Micho Garcia <michogar@gmail.com>
2016-12-29 16:25:31 +0100

    Added:
    * webpack
    * babel
    * eslint
    
    and created project folder structured

---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var entryPath = path.join(__dirname, 'src'),
+    outputPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    devtool: 'cheap-eval-source-map',
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        path.join(entryPath, 'index.js')
+    ],
+    output: {
+        path: outputPath,
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+          {test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/}
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                include: entryPath,
+                loaders: ['babel']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: outputPath
+    }
+};
+
---
+++ webpack.config.prod.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var entryPath = path.join(__dirname, 'src'),
+    outputPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        path.join(entryPath, 'index.js')
+    ],
+    output: {
+        path: outputPath,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: entryPath,
+                loaders: ['babel']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: outputPath
+    }
+};
+
