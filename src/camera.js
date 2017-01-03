685c0844f8878e91a10468b7ec9fe83ed2d38625
Micho Garcia <michogar@gmail.com>
2017-01-03 13:36:02 +0100

    Added showCamera and removeCamera methods

--- src/camera.js
+++ src/camera.js
@@ -12,8 +12,48 @@ class Camera {
     }
   }
 
-  getPicture(successCallback, errorCallback, options) {
+  showCamera() {
+    return new Promise((resolve, reject) => {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        this.video = document.createElement('video')
+        this.video.setAttribute('id', 'video')
+        this.video.setAttribute('width', '640')
+        this.video.setAttribute('height', '480')
+        this.video.setAttribute('autoplay', true)
 
+        const snap = document.createElement('button')
+        snap.setAttribute('id', 'snap')
+        snap.appendChild(document.createTextNode('Snap Photo'))
+
+        const canvas = document.createElement('canvas')
+        canvas.setAttribute('id', 'canvas')
+        canvas.setAttribute('width', '640')
+        canvas.setAttribute('height', '480')
+
+        document.body.appendChild(this.video)
+        document.body.appendChild(snap)
+        document.body.appendChild(canvas)
+
+        this.video.src = window.URL.createObjectURL(stream)
+        this.video.play()
+        resolve()
+      }).catch((err) => {
+        this.removeCamera()
+        reject(err)
+      })
+    })
+  }
+
+  removeCamera() {
+    const snap = document.getElementById('snap')
+    const canvas = document.getElementById('canvas')
+    document.body.removeChild(this.video)
+    document.body.removeChild(snap)
+    document.body.removeChild(canvas)
+  }
+
+  getPicture(successCallback, errorCallback, options) {
+    this.showCamera()
   }
 }
 
--- tests/cameraSpec.js
+++ tests/cameraSpec.js
@@ -4,6 +4,7 @@
 
 import Camera from '../src/camera'
 
+
 describe('Camera', () => {
   it('should not instantiate without media', () => {
     let camera
@@ -18,5 +19,43 @@ describe('Camera', () => {
       expect(error).not.toBeDefined()
     }
     expect(camera).toBeDefined()
+  })
+
+  const camera = new Camera()
+  it('should show dialog with camera', () => {
+    camera.showCamera().then((done) => {
+      const video = document.getElementById('video')
+      expect(video).not.toBeNull()
+      expect(video.getAttribute('id')).toBe('video')
+      expect(video.getAttribute('width')).toBe('640')
+
+      const snap = document.getElementById('snap')
+      expect(snap).not.toBeNull()
+      expect(snap.getAttribute('id')).toBe('snap')
+
+      const canvas = document.getElementById('canvas')
+      expect(canvas).not.toBeNull()
+      expect(canvas.getAttribute('id')).toBe('canvas')
+      done()
+    }).catch((done) => {
+      expect(true).toBeFalsy()
+      done()
+    })
+  })
+
+  it('should remove dialog with camera', () => {
+    camera.showCamera().then((done) => {
+      camera.removeCamera()
+
+      const video = document.getElementById('video')
+      expect(video).toBeNull()
+
+      const snap = document.getElementById('snap')
+      expect(snap).toBeNull()
+
+      const canvas = document.getElementById('canvas')
+      expect(canvas).toBeNull()
+      done()
+    })
   })
 })
