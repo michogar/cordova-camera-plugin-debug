a9e111130801e24147ed8a22952cfa3b5e6e263e
Micho Garcia <michogar@gmail.com>
2017-01-04 16:33:45 +0100

    Changed indent in webpack files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,27 +1,27 @@
 var path = require('path');
 var entryPath = path.join(__dirname, 'src'),
-    outputPath = path.join(__dirname, 'dist');
+  outputPath = path.join(__dirname, 'dist');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: [
-        path.join(entryPath, 'index.js')
-    ],
-    output: {
-        path: outputPath,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: entryPath,
-                loaders: ['babel']
-            }
-        ]
-    },
-    devServer: {
-        contentBase: outputPath
-    }
+  devtool: 'source-map',
+  entry: [
+    path.join(entryPath, 'index.js')
+  ],
+  output: {
+    path: outputPath,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: entryPath,
+        loaders: ['babel']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: outputPath
+  }
 };
 
